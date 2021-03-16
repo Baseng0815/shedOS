56173a71d4457b6cf19f6b45c181031d59fdc48d
Baseng0815 <bastian.engel00@gmail.com>
2021-03-16 06:08:00 +0100

    add enable-kvm and host cpu to qemu

--- qemu.sh
+++ qemu.sh
@@ -3,5 +3,5 @@ set -e
 . ./config.sh
 
 qemu-system-x86_64 -drive if=pflash,format=raw,file=$EXTERNAL/OVMF.fd \
-    -drive format=raw,file=fat:rw:$HDDDIR -M accel=kvm:tcg \
+    -drive format=raw,file=fat:rw:$HDDDIR -enable-kvm -cpu host \
     -net none -serial stdio
