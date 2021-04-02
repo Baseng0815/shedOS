27bdf55e9fb10bf88fd8105e254a2dfe60c189f5
Baseng0815 <bastian.engel00@gmail.com>
2021-04-02 18:48:13 +0200

    remove files

--- config.sh
+++ config.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 # all those flags DO NOT apply to the bootloader!
-export CFLAGS="-O2 -Wall -Wextra -ggdb -std=gnu11"
 
 # we use a generic x86_64 target using the ELF format
-export TARGET=x86_64-elf
-export CC=$TARGET-gcc
-export AS=$TARGET-as
-export LD=$TARGET-ld
-export AR=$TARGET-ar
-
-export PROJECTS="libc kernel loader"
-export SYSTEM_HEADER_PROJECTS="libc kernel"
-export SYSROOT=$(pwd)/sysroot
-export ISODIR=$(pwd)/iso
-export EXTERNAL=$(pwd)/external
 
 # all projects will be gradually installed into the sysroot
 export CC="$CC --sysroot=$SYSROOT"
--- kernel/src/int/irqs.h
+++
@@ -1,7 +0,0 @@
-#ifndef _IRQS_H
-#define _IRQS_H
-
-struct interrupt_frame;
-__attribute__((interrupt)) void kb_handle(struct interrupt_frame*);
-
-#endif
