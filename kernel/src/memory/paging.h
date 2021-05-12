8194efb6e8672aae229bf2976617ca0316711170
Baseng0815 <bastian.engel00@gmail.com>
2021-05-12 13:53:55 +0200

    make pt_entry bitmap at least somewhat better

--- kernel/src/memory/paging.h
+++ kernel/src/memory/paging.h
@@ -15,17 +15,17 @@
    */
 
 struct pt_entry {
-        bool present        : 1; /* 1 => page is in physical memory */
-        bool writable       : 1; /* 1 => read/write, 0 => read only */
-        bool user_access    : 1; /* 1 => page can be accessed by all */
-        bool write_through  : 1; /* 1 => use write-through cache strategy */
-        bool cache_disabled : 1; /* 1 => page will not be cached */
-        bool accessed       : 1; /* set to 1 when page is accessed */
-        bool ignore0        : 1; /* can't use, is reserved */
-        bool large_pages    : 1; /* 0 => 4KiB page size */
-        bool ignore1        : 1; /* can't use, is reserved */
-        uint8_t available   : 3; /* here we can store anything we want */
-        uintptr_t addr      : 52; /* either physical memory or other pd_entry */
+        uint64_t present        : 1; /* 1 => page is in physical memory */
+        uint64_t writable       : 1; /* 1 => read/write, 0 => read only */
+        uint64_t user_access    : 1; /* 1 => page can be accessed by all */
+        uint64_t write_through  : 1; /* 1 => use write-through cache strategy */
+        uint64_t cache_disabled : 1; /* 1 => page will not be cached */
+        uint64_t accessed       : 1; /* set to 1 when page is accessed */
+        uint64_t ignore0        : 1; /* can't use, is reserved */
+        uint64_t large_pages    : 1; /* 0 => 4KiB page size */
+        uint64_t ignore1        : 1; /* can't use, is reserved */
+        uint64_t available      : 3; /* here we can store anything we want */
+        uint64_t addr           : 52; /* either physical memory or other pd_entry */
 };
 
 /* a page table is exactly 4096 bytes large so it fits into a single frame */
